b688498b646f932e8a8c4c883baf0338707f1e7f
Anjaneya Tripathi <72185645+anjaneyatripathi1995@users.noreply.github.com>
2026-03-06 21:18:37 +0530

    Delete routes directory

--- routes/api.php
+++
@@ -1,8 +0,0 @@
-use App\Http\Controllers\Api\EmiController;
-use App\Http\Controllers\Api\ToolJobController;
-
-Route::post('/emi', [EmiController::class, 'calculate']);
-
-// generic tool job endpoints (merge, split, compress, pdf->image, etc.)
-Route::post('tools/{slug}', [ToolJobController::class, 'store']);
-Route::get('jobs/{id}', [ToolJobController::class, 'show']);
